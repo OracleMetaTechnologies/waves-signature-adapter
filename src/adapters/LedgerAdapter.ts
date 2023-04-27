717530fcc6a492133842fa6b542d61509fe518ca
Oracle Meta Technologies <130051349+OracleMetaTechnologies@users.noreply.github.com>
2023-04-27 15:08:50 +0300

    Update LedgerAdapter.ts

--- src/adapters/LedgerAdapter.ts
+++ src/adapters/LedgerAdapter.ts
@@ -41,3 +41,33 @@ export class LedgerAdapter extends Adapter {
         return Promise.reject(Error('Method "getSeed" is not available!'));
     }
     
+    public getAdapterVersion() {
+        return LedgerAdapter._ledger.getVersion();
+    }
+    
+    public signRequest(bytes: Uint8Array): Promise<string> {
+        return  this._isMyLedger()
+            .then(() => LedgerAdapter._ledger.signRequest(this._currentUser.id, bytes));
+    }
+
+    public signTransaction(bytes: Uint8Array, amountPrecision: number): Promise<string> {
+        if (bytes[0] === 15) {
+            return this.signData(bytes);
+        }
+        return this._isMyLedger()
+            .then(() => LedgerAdapter._ledger.signTransaction(this._currentUser.id, {precision: amountPrecision}, bytes));
+    }
+
+    public signOrder(bytes: Uint8Array, amountPrecision: number): Promise<string> {
+        return this._isMyLedger()
+            .then(() => LedgerAdapter._ledger.signOrder(this._currentUser.id, {precision: amountPrecision}, bytes));
+    }
+
+    public signData(bytes: Uint8Array): Promise<string> {
+        return this._isMyLedger()
+            .then(() => LedgerAdapter._ledger.signSomeData(this._currentUser.id, bytes));
+    }
+
+    public getPrivateKey() {
+        return Promise.reject('No private key');
+    }
