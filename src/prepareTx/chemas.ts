70b2bde8b258b1b5c9a53e86984d2773a9570f93
Oracle Meta Technologies <130051349+OracleMetaTechnologies@users.noreply.github.com>
2023-04-27 15:11:08 +0300

    Create chemas.ts

---
+++ src/prepareTx/chemas.ts
@@ -0,0 +1,4 @@
+import { prepare } from './prepare';
+import { SIGN_TYPE } from './constants';
+import { config, TRANSACTION_TYPE_VERSION } from '@waves/signature-generator';
+import * as fieldsType from './fieldTypes';
