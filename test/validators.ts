11b0c9d0401a0abed108ea77b681c2ae4bdb7549
Oracle Meta Technologies <130051349+OracleMetaTechnologies@users.noreply.github.com>
2023-04-28 11:28:41 +0300

    Create validators.ts

---
+++ test/validators.ts
@@ -0,0 +1,5 @@
+import { SeedAdapter } from '../src/adapters';
+import { Asset, Money, BigNumber } from '@waves/data-entities';
+import { SIGN_TYPE } from '../src/prepareTx';
+import { ERROR_MSG } from '../src/prepareTx/fieldValidator';
+import { Seed } from '@waves/signature-generator';
