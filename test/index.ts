d7b472a85be709efa0ecf2b21596c111c1b21905
Oracle Meta Technologies <130051349+OracleMetaTechnologies@users.noreply.github.com>
2023-04-28 11:39:53 +0300

    Update index.ts

--- test/index.ts
+++ test/index.ts
@@ -3,3 +3,7 @@ import { Asset, BigNumber, Money } from '@waves/data-entities';
 import { SeedAdapter, SIGN_TYPE } from '../src/index';
 import './WavesKeeperAdapter';
 import './validators';
+
+
+const testSeed = 'some test seed words without money on mainnet';
+const seed = new Seed(testSeed);
