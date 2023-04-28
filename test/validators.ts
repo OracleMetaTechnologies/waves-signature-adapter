04eaadc29fb95db53704bd531dfa91176641348b
Oracle Meta Technologies <130051349+OracleMetaTechnologies@users.noreply.github.com>
2023-04-28 11:29:15 +0300

    Update validators.ts

--- test/validators.ts
+++ test/validators.ts
@@ -3,3 +3,19 @@ import { Asset, Money, BigNumber } from '@waves/data-entities';
 import { SIGN_TYPE } from '../src/prepareTx';
 import { ERROR_MSG } from '../src/prepareTx/fieldValidator';
 import { Seed } from '@waves/signature-generator';
+
+const getError = (e: Error) => JSON.parse(e.message);
+const testSeed = 'some test seed words without money on mainnet';
+const seed = new Seed(testSeed);
+const testAsset = new Asset({
+    precision: 5,
+    id: 'Gtb1WRznfchDnTh37ezoDTJ4wcoKaRsKqKjJjy7nm2zU',
+    quantity: new BigNumber(10000),
+    description: 'Some text',
+    height: 100,
+    name: 'Test',
+    reissuable: false,
+    sender: seed.address,
+    timestamp: new Date(),
+    ticker: undefined
+});
