d1e8609c2d22eba882694af1e153d69cf10f03ac
Oracle Meta Technologies <130051349+OracleMetaTechnologies@users.noreply.github.com>
2023-04-28 10:39:21 +0300

    Update fieldValidator.ts

--- src/prepareTx/fieldValidator.ts
+++ src/prepareTx/fieldValidator.ts
@@ -20,3 +20,25 @@ const ASSETS = {
     NAME_MAX_BYTES: 16,
     DESCRIPTION_MAX_BYTES: 1000,
 };
+
+export const ERROR_MSG = {
+    REQUIRED: 'field is required',
+    WRONG_TYPE: 'field is wrong type',
+    WRONG_NUMBER: 'field is not number',
+    WRONG_TIMESTAMP: 'field is not timestamp',
+    SMALL_FIELD: 'field is small',
+    LARGE_FIELD: 'field is large',
+    WRONG_SYMBOLS: 'field has wrong symbols',
+    WRONG_ADDRESS: 'field is wrong address',
+    WRONG_BOOLEAN: 'field is wrong boolean',
+    WRONG_ASSET_ID: 'field is wrong assetId',
+    WRONG_ORDER_TYPE: 'field is wrong order type. Field can be "buy" or "sell"',
+    NOT_HTTPS_URL: 'field can be url with https protocol',
+    BASE64: 'field can be base64 string with prefix "base64:"',
+    EMPTY_BASE64: 'field can be not empty base64"',
+};
+
+const isBase64 = (value: string): boolean => {
+    const regExp = /^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$/;
+    return regExp.test(value);
+};
