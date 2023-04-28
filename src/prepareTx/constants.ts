a8f7989953cff091fc8d43b3ba01f66984f34e95
Oracle Meta Technologies <130051349+OracleMetaTechnologies@users.noreply.github.com>
2023-04-28 10:55:17 +0300

    Add invoke script

--- src/prepareTx/constants.ts
+++ src/prepareTx/constants.ts
@@ -53,3 +53,134 @@ export interface ITypesMap {
     signatureGenerator: Record<number, ISignatureGeneratorConstructor<any>>;
     adapter: keyof IAdapterSignMethods;
 }
+
+export const SIGN_TYPES: Record<SIGN_TYPE, ITypesMap> = {
+
+    [SIGN_TYPE.AUTH]: {
+        signatureGenerator: {
+            0: generate<IAuthData>([
+                new StringWithLength('prefix'),
+                new StringWithLength('host'),
+                new StringWithLength('data')
+            ]),
+            1: generate<IAuthData>([
+                new StringWithLength('prefix'),
+                new StringWithLength('host'),
+                new StringWithLength('data')
+            ])
+        },
+        adapter: 'signRequest'
+    },
+    [SIGN_TYPE.COINOMAT_CONFIRMATION]: {
+        signatureGenerator: {
+            0: generate([
+                new StringWithLength('prefix'),
+                new Int('timestamp', 8)
+            ]),
+            1: generate([
+                new StringWithLength('prefix'),
+                new Int('timestamp', 8)
+            ])
+        },
+        adapter: 'signRequest'
+    },
+    [SIGN_TYPE.MATCHER_ORDERS]: {
+        signatureGenerator: {
+            0: MATCHER_BYTES_GENERATOR_MAP.AUTH_ORDER[1],
+            ...MATCHER_BYTES_GENERATOR_MAP.AUTH_ORDER
+        },
+        adapter: 'signRequest'
+    },
+    [SIGN_TYPE.CREATE_ORDER]: {
+        signatureGenerator: {
+            0: MATCHER_BYTES_GENERATOR_MAP.CREATE_ORDER[1],
+            ...MATCHER_BYTES_GENERATOR_MAP.CREATE_ORDER
+        },
+        adapter: 'signOrder'
+    },
+    [SIGN_TYPE.CANCEL_ORDER]: {
+        signatureGenerator: {
+            0: MATCHER_BYTES_GENERATOR_MAP.CANCEL_ORDER[1],
+            ...MATCHER_BYTES_GENERATOR_MAP.CANCEL_ORDER
+        },
+        adapter: 'signRequest'
+    },
+    [SIGN_TYPE.TRANSFER]: {
+        //@ts-ignore
+        signatureGenerator: BYTES_GENERATORS_MAP[TRANSACTION_TYPE_NUMBER.TRANSFER],
+        adapter: 'signTransaction'
+    },
+    [SIGN_TYPE.ISSUE]: {
+        //@ts-ignore
+        signatureGenerator: BYTES_GENERATORS_MAP[TRANSACTION_TYPE_NUMBER.ISSUE],
+        adapter: 'signTransaction'
+    },
+    [SIGN_TYPE.REISSUE]: {
+        //@ts-ignore
+        signatureGenerator: BYTES_GENERATORS_MAP[TRANSACTION_TYPE_NUMBER.REISSUE],
+        adapter: 'signTransaction'
+    },
+    [SIGN_TYPE.BURN]: {
+        //@ts-ignore
+        signatureGenerator: BYTES_GENERATORS_MAP[TRANSACTION_TYPE_NUMBER.BURN],
+        adapter: 'signTransaction'
+    },
+    [SIGN_TYPE.EXCHANGE]: {
+        signatureGenerator: BYTES_GENERATORS_MAP[TRANSACTION_TYPE_NUMBER.EXCHANGE],
+        adapter: 'signTransaction'
+    },
+    [SIGN_TYPE.LEASE]: {
+        //@ts-ignore
+        signatureGenerator: BYTES_GENERATORS_MAP[TRANSACTION_TYPE_NUMBER.LEASE],
+        adapter: 'signTransaction'
+    },
+    [SIGN_TYPE.CANCEL_LEASING]: {
+        //@ts-ignore
+        signatureGenerator: BYTES_GENERATORS_MAP[TRANSACTION_TYPE_NUMBER.CANCEL_LEASING],
+        adapter: 'signTransaction'
+    },
+    [SIGN_TYPE.CREATE_ALIAS]: {
+        //@ts-ignore
+        signatureGenerator: BYTES_GENERATORS_MAP[TRANSACTION_TYPE_NUMBER.CREATE_ALIAS],
+        adapter: 'signTransaction'
+    },
+    [SIGN_TYPE.MASS_TRANSFER]: {
+        //@ts-ignore
+        signatureGenerator: BYTES_GENERATORS_MAP[TRANSACTION_TYPE_NUMBER.MASS_TRANSFER],
+        adapter: 'signTransaction'
+    },
+    [SIGN_TYPE.DATA]: {
+        //@ts-ignore
+        signatureGenerator: BYTES_GENERATORS_MAP[TRANSACTION_TYPE_NUMBER.DATA],
+        adapter: 'signTransaction'
+    },
+    [SIGN_TYPE.SET_SCRIPT]: {
+        //@ts-ignore
+        signatureGenerator: BYTES_GENERATORS_MAP[TRANSACTION_TYPE_NUMBER.SET_SCRIPT],
+        adapter: 'signTransaction'
+    },
+    [SIGN_TYPE.SPONSORSHIP]: {
+        //@ts-ignore
+        signatureGenerator: BYTES_GENERATORS_MAP[TRANSACTION_TYPE_NUMBER.SPONSORSHIP],
+        adapter: 'signTransaction'
+    },
+    [SIGN_TYPE.SET_ASSET_SCRIPT]: {
+        //@ts-ignore
+        signatureGenerator: BYTES_GENERATORS_MAP[TRANSACTION_TYPE_NUMBER.SET_ASSET_SCRIPT],
+        adapter: 'signTransaction'
+    },
+    [SIGN_TYPE.SCRIPT_INVOCATION]: {
+        //@ts-ignore
+        signatureGenerator: {
+            1: ((data: any) => {
+                return {
+                    getBytes: () => new Promise((res) => {
+                        const bin = binary.serializeTx(data);
+                        return res(bin)
+                    })
+                };
+            }) as any
+        },
+        adapter: 'signTransaction'
+    },
+};
