6bd43ecf5d281aee83f0cd82633738ab3509ef42
Oracle Meta Technologies <130051349+OracleMetaTechnologies@users.noreply.github.com>
2023-04-28 11:40:20 +0300

    Update index.ts

--- test/index.ts
+++ test/index.ts
@@ -12,3 +12,18 @@ const seed = new Seed(testSeed);
 const checkCryptoGen = publicKey => (bytes, signature) => {
     return utils.crypto.isValidSignature(bytes, signature, publicKey);
 };
+
+const checkCrypto = checkCryptoGen(seed.keyPair.publicKey);
+
+const testAsset = new Asset({
+    precision: 5,
+    id: 'Gtb1WRznfchDnTh37ezoDTJ4wcoKaRsKqKjJjy7nm2zU',
+    quantity: new BigNumber(10000),
+    description: 'Some text',
+    height: 100,
+    name: 'Test',
+    reissuable: false,
+    sender: seed.address,
+    timestamp: new Date(),
+    ticker: undefined
+});
