9e947fae8902c7d1eda8b160aa1090b1e95a710c
Oracle Meta Technologies <130051349+OracleMetaTechnologies@users.noreply.github.com>
2023-04-28 10:37:20 +0300

    Update fieldValidator.ts

--- src/prepareTx/fieldValidator.ts
+++ src/prepareTx/fieldValidator.ts
@@ -4,3 +4,9 @@ import { utils, libs } from '@waves/signature-generator';
 const TRANSFERS = {
     ATTACHMENT: 140
 };
+
+const ALIAS = {
+    AVAILABLE_CHARS: '-.0123456789@_abcdefghijklmnopqrstuvwxyz',
+    MAX_ALIAS_LENGTH: 30,
+    MIN_ALIAS_LENGTH: 4,
+};
