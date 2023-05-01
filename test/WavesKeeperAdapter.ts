c4af122979eee85af8ce5e2f6e1505d448a963c8
Oracle Meta Technologies <130051349+OracleMetaTechnologies@users.noreply.github.com>
2023-05-01 12:25:30 +0300

    Create WavesKeeperAdapter.ts

---
+++ test/WavesKeeperAdapter.ts
@@ -0,0 +1,3 @@
+import { WavesKeeperAdapter } from '../src/adapters/WavesKeeperAdapter';
+import { Asset, Money, BigNumber } from '@waves/data-entities';
+import { TRANSACTION_TYPE_NUMBER } from '../src/prepareTx';
